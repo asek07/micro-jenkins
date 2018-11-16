e5423cc855cacec2a59380edf667eca155d89b35
Andy Sekulovski <andy.sek94@gmail.com>
2018-11-16 11:38:28 +1100

    initial commit

---
+++ src/main/java/com/asek/micro_app/MicroAppApplication.java
@@ -0,0 +1,12 @@
+package com.asek.micro_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MicroAppApplication.class, args);
+    }
+}
---
+++ src/test/java/com/asek/micro_app/MicroAppApplicationTests.java
@@ -0,0 +1,16 @@
+package com.asek.micro_app;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MicroAppApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
